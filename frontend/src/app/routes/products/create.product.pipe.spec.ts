007a3bd7ba9c50f9f18f0ca64becd3c850765aa5
alamin <garogigi@gmail.com>
2024-11-12 09:01:11 +0300

    create home component

---
+++ frontend/src/app/routes/products/create.product.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CreateProductPipe } from './create.product.pipe';
+
+describe('CreateProductPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CreateProductPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
