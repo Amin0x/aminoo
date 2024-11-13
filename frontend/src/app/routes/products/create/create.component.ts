099f6a1a67cf5947df08a8219ee35a02934eaa67
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-13 17:51:03 +0300

    Update create.component.ts

--- frontend/src/app/routes/products/create/create.component.ts
+++ frontend/src/app/routes/products/create/create.component.ts
@@ -9,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class CreateComponent {
 
+  @Output() fileSelected = new EventEmitter<File>();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.target as HTMLElement;
+    dropZone.classList.add('drop-zone--over');
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.target as HTMLElement;
+    dropZone.classList.remove('drop-zone--over');
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    const dropZone = event.target as HTMLElement;
+    dropZone.classList.remove('drop-zone--over');
+    const file = event.dataTransfer.files[0];
+    this.fileSelected.emit(file);
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    this.fileSelected.emit(file);
+  }
+
 }
