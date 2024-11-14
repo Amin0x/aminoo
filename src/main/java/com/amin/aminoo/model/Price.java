a6a4d2f364e3232478360c823c0d092e0990ffdb
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-14 18:55:10 +0300

    Create Price.java

---
+++ src/main/java/com/amin/aminoo/model/Price.java
@@ -0,0 +1,22 @@
+package com.ameen.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Price {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long price;
+    private Long priceUsd;
+
+    // other fields, getters, and setters
+}
