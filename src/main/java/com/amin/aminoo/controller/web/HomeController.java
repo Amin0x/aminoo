64228fdfd4fb51d15a1806c32b484a3c068d021c
Amin <49003095+Amin0x@users.noreply.github.com>
2024-11-21 00:49:27 +0300

    Update HomeController.java

--- src/main/java/com/amin/aminoo/controller/web/HomeController.java
+++ src/main/java/com/amin/aminoo/controller/web/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/users")
+@Controller
+@RequestMapping("/web/users")
 public class HomeController {
 
     @Autowired
